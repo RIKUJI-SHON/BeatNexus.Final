433410ae7ed3b50e85455633c95d06cb286b0376
RIKUJI <rikujiogino@gmail.com>
2025-06-17 22:32:39 +0900

    バトルカード上の投票ボタン

--- src/components/ui/VoteButton.tsx
+++ src/components/ui/VoteButton.tsx
@@ -14,25 +14,15 @@ export const VoteButton: React.FC<VoteButtonProps> = ({
   className = '' 
 }) => {
   return (
-    <button
-      onClick={onClick}
-      disabled={disabled}
-      className={`
-        vote-btn
-        ${disabled ? 'opacity-50 cursor-not-allowed' : 'cursor-pointer'}
-        ${className}
-      `}
-    >
-      <div id="container-stars">
-        <div id="stars"></div>
-      </div>
-      
-      <div id="glow">
-        <div className="circle"></div>
-        <div className="circle"></div>
-      </div>
-      
-      <strong>{children}</strong>
-    </button>
+    <div className={`vote-button-container ${className}`}>
+      <button
+        onClick={onClick}
+        disabled={disabled}
+        className="vote-space-button"
+      >
+        <span>{children}</span>
+        <div className="bright-particles"></div>
+      </button>
+    </div>
   );
 }; 
